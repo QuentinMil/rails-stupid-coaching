482b4cfd8e1128ad1f526823cd73f3b54aa7b09b
(Quentin Milhomme) <milhommequentin@yahoo.fr>
2024-07-01 17:07:50 +0200

    exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask'
+
+  class QuestionsController < ApplicationController
+    def ask
+    end
+
+    def answer
+      @question = params[:question]
+      if @question == "I am going to work"
+        @answer = "Great!"
+      elsif @question.end_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
